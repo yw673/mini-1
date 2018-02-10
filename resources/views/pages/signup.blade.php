0d92fb4474f55d87603354ba07c2a49948b37ff1
yangyin <yw673@njit.edu>
2018-02-10 06:58:26 -0500

    Changing the register page

--- resources/views/pages/signup.blade.php
+++ resources/views/pages/signup.blade.php
@@ -7,7 +7,7 @@
             <img class="mb-4" src="https://getbootstrap.com/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
             <h1 class="h3 mb-3 font-weight-normal">Register</h1>
             <label for="inputEmail" class="sr-only">Email address</label>
-            <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+            <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus >
             <label for="inputPassword" class="sr-only">Password</label>
             <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
             <label for="inputPassword" class="sr-only">Confirm</label>
