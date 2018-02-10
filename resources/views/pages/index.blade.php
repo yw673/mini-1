a362c530c047b242b30fc251be33168cddf5acc4
yangyin <yw673@njit.edu>
2018-02-10 06:29:35 -0500

    ok

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -31,7 +31,7 @@
             </div>
             <div class="col-md-4">
                 <h2>Heading</h2>
-                <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula
+                <p>Donec sed odio dui. Cras justo odio, dapjiibus ac facilisis in, egestas eget quam. Vestibulum id ligula
                     porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh,
                     ut fermentum massa justo sit amet risus.</p>
                 <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
